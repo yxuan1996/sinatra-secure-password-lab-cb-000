d3cd9cfae149c9012e9481b5bf42758dc63b024b
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-14 23:03:12 +0000

    automatically backed up by learn

--- db/migrate/20171014225437_create_users.rb
+++ db/migrate/20171014225437_create_users.rb
@@ -1,15 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    def up
-	   create_table :users do |t|
-	      t.string :username
-	      t.string :password_digest
-	   end
-	  end
-
-	  def down
-	    drop_table :users
-	  end
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+    end
 
   end
 end

fe181f9954bde3e9659065644d8f8665964271ef
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-14 23:05:31 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,11 +17,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    user = User.new(:username => params[:username], :password => params[:password])
-    if user.save
-      redirect '/login'
-    else
+    if params[:username] == "" || params[:password] == ""
       redirect '/failure'
+    else
+      user = User.new(:username => params[:username], :password => params[:password])
+      user.save
+      redirect '/login'
     end
 
   end
